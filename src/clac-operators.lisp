14cd83fc4ed2276ecfb1dcbd8080c294558bd39f
Hamza Shahid <hamzashahid.blit.github@protonmail.com>
2022-06-23 11:50:43 +0500

    Fixed *CLAC-OPS*

--- src/clac-operators.lisp
+++ src/clac-operators.lisp
@@ -1,9 +1,10 @@
 (in-package :claculator)
 
-(defun gen-define-operators (name &rest tokens)
-  "Defines Operators with the symbol NAME and tokens, TOKENS."
-  `(defparameter ,name
-     (quote ,@tokens))) 
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun gen-define-operators (name &rest tokens)
+    "Defines Operators with the symbol NAME and tokens, TOKENS."
+    `(defparameter ,name
+       (quote ,@tokens)))) 
 
 (defmacro define-operators (name (key value) &rest other-pairs)
   (gen-define-operators name (cons (list key value) other-pairs)))
@@ -26,12 +27,11 @@
 (defmethod percentage (arg)
   (/ arg 100))
 
-;;; TODO: FIX; Gives error as gen-define-operators isn't compiled before macro expansion
-;; (define-operators *clac-ops*
-;;   ("+" +)
-;;   ("-" -)
-;;   ("*" *)
-;;   ("/" /)
-;;   ("%" percentage)
-;;   ("mod" mod)
-;;   ("rem" rem))
+(define-operators *clac-ops*
+  ("+" +)
+  ("-" -)
+  ("*" *)
+  ("/" /)
+  ("%" percentage)
+  ("mod" mod)
+  ("rem" rem))
